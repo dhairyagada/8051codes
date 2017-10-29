1c9eee2af03b1e322219a50c77c9ce96d397f5c8
Dhairya <dhairyagada1997@gmail.com>
2017-10-29 20:20:20 +0530

    SQ wave using timer

---
+++ 8/Code.asm
@@ -0,0 +1,20 @@
+org	0x00
+			mov a,#0ffh
+again:		mov p1,a
+			cpl a
+			acall delay
+			sjmp again
+	
+org	0x300		
+Delay:		
+			mov tmod,#01h						;Specifies timer 0 is to be used in mode 0
+			mov tl0,#0afh
+			mov th0,#0ffh						;Timer Values for 5 Khz frequency at Crystal of 11.0592 MHz
+												;Change this values to change the frequency of wave
+			setb tr0							;Starts timer 0
+			up:
+			jbc tf0,over						;
+			sjmp up								;Monitors Timer 0 flag to become high and then makes it zero again
+			over:
+			ret
+end		
