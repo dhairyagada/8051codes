2759ff67e2f68d1471343e1e9b10980b7ae60e33
Dhairya <dhairyagada1997@gmail.com>
2017-10-30 12:53:55 +0530

    Timer Interrupt

---
+++ 10/Code.asm
@@ -0,0 +1,20 @@
+ORG 0000H
+	LJMP Main
+	
+ORG 300H
+	Main:
+	MOV IE,#82H
+	MOV TMOD,#02H
+	MOV TH1,#0A3H
+	SETB TR0
+	MOV P0,#00H
+	;mov P3,#00H
+	SJMP $
+	
+
+ORG 000BH
+	CPL P0.0
+	clr TF0
+	RETI
+
+END
